25dde0858fd9b16b17e7e415d70cc400b7fa680c
Nathan Witt <wittnl@gmail.com>
2014-09-25 17:03:22 -0500

    Build configs

--- build.js
+++ build.js
@@ -1,3 +1,12 @@
 /**
- * Created by nathan on 9/25/14.
+ * flash-audio build script
+ *
+ * Working command
+ * node_modules/flex-sdk/bin/mxmlc src/FlashAudio.as -o flash-audio2.swf
  */
+var exec = require('child_process').exec;
+
+exec('mxmlc', function(err, stdout, stderr) {
+    if(err) throw err;
+    else console.log(stdout);
+});
